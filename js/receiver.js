07f525fa30bf22efb4dcbac21fc09bed3faca4cb
geniusburger <jdst8-git@yahoo.com>
2014-05-30 21:10:34 -0700

    Updated root objects

--- js/receiver.js
+++ js/receiver.js
@@ -1,21 +1,24 @@
-function castInit() {
+rcvr = {};
+
+rcvr.clock = null;
+
+rcvr.namespace = 'urn:x-cast:me.geniusburger.cast.wordclock';
+
+rcvr.castInit = function () {
     cast.receiver.logger.setLevelValue(0);
     window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
     console.log('Starting Receiver Manager');
 
-    // handler for the 'ready' event
     castReceiverManager.onReady = function (event) {
         console.log('Received Ready event: ' + JSON.stringify(event.data));
         window.castReceiverManager.setApplicationState("Application status is ready...");
     };
 
-    // handler for 'senderconnected' event
     castReceiverManager.onSenderConnected = function (event) {
         console.log('Received Sender Connected event: ' + event.data);
         console.log(window.castReceiverManager.getSender(event.data).userAgent);
     };
 
-    // handler for 'senderdisconnected' event
     castReceiverManager.onSenderDisconnected = function (event) {
         console.log('Received Sender Disconnected event: ' + event.data);
         if (window.castReceiverManager.getSenders().length == 0) {
@@ -23,22 +26,19 @@ function castInit() {
         }
     };
 
-    // handler for 'systemvolumechanged' event
     castReceiverManager.onSystemVolumeChanged = function (event) {
-        console.log('Received System Volume Changed event: ' + event.data['level'] + ' ' +
-            event.data['muted']);
+        console.log('Received System Volume Changed event: ' + event.data['level'] + ' ' + event.data['muted']);
     };
 
     // create a CastMessageBus to handle messages for a custom namespace
-    window.messageBus =
-        window.castReceiverManager.getCastMessageBus(
-            'urn:x-cast:com.google.cast.sample.helloworld');
+    window.messageBus = window.castReceiverManager.getCastMessageBus(rcvr.namespace);
 
     // handler for the CastMessageBus message event
     window.messageBus.onMessage = function (event) {
         console.log('Message [' + event.senderId + ']: ' + event.data);
         // display the message from the sender
-        displayText(event.data);
+        rcvr.clock.updateSettings(event.data);
+        window.castReceiverManager.setApplicationState(JSON.stringify(event.data));
         // inform all senders on the CastMessageBus of the incoming message event
         // sender message listener will be invoked
         window.messageBus.send(event.senderId, event.data);
@@ -49,23 +49,17 @@ function castInit() {
     console.log('Receiver Manager started');
 };
 
-// utility function to display the text message in the input field
-function displayText(text) {
-    console.log(text);
-    document.getElementById("message").innerHTML = text;
-    window.castReceiverManager.setApplicationState(text);
-}
+rcvr.receiverInit = function () {
+    rcvr.clock = new Clock(document.getElementById('clock'));
+    castInit();
+};
 
-function receiverInit() {
-    var clock = new Clock(document.getElementById('clock'));
-}
-
-function addEvent(el, evnt, func) {
-    if(el.addEventListener) {
+rcvr.addEvent = function (el, evnt, func) {
+    if (el.addEventListener) {
         el.addEventListener(evnt, func, false);
-    } else if(el.attachEvent) {
-        el.attachEvent('on'+evnt, func);
+    } else if (el.attachEvent) {
+        el.attachEvent('on' + evnt, func);
     }
-}
+};
 
-addEvent(window, 'load', receiverInit);
+rcvr.addEvent(window, 'load', receiverInit);
--- js/sender.js
+++ js/sender.js
@@ -1,164 +1,144 @@
-var applicationID = '794B7BBF';
-var namespace = 'urn:x-cast:com.google.cast.sample.helloworld';
-var session = null;
+sender = {};
+sender.applicationID = '794B7BBF';
+sender.namespace = 'urn:x-cast:me.geniusburger.cast.wordclock';
+sender.session = null;
 
 /**
  * Call initialization for Cast
  */
 if (!chrome.cast || !chrome.cast.isAvailable) {
-    setTimeout(initializeCastApi, 1000);
+    setTimeout(sender.initializeCastApi, 1000);
 }
 
 /**
  * initialization
  */
-function initializeCastApi() {
-    var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+sender.initializeCastApi = function() {
+    var sessionRequest = new chrome.cast.SessionRequest(sender.applicationID);
     var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
         sessionListener,
         receiverListener);
 
-    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
-}
-;
+    chrome.cast.initialize(apiConfig, sender.onInitSuccess, sender.onError);
+};
 
 /**
  * initialization success callback
  */
-function onInitSuccess() {
-    appendMessage("onInitSuccess");
-}
+sender.onInitSuccess = function() {
+    sender.appendMessage("onInitSuccess");
+};
 
 /**
  * initialization error callback
  */
-function onError(message) {
-    appendMessage("onError: " + JSON.stringify(message));
-}
+sender.onError = function(message) {
+    sender.appendMessage("onError: " + JSON.stringify(message));
+};
 
 /**
  * generic success callback
  */
-function onSuccess(message) {
-    appendMessage("onSuccess: " + message);
-}
+sender.onSuccess = function(message) {
+    sender.appendMessage("onSuccess: " + message);
+};
 
 /**
  * callback on success for stopping app
  */
-function onStopAppSuccess() {
-    appendMessage('onStopAppSuccess');
-}
+sender.onStopAppSuccess = function() {
+    sender.appendMessage('onStopAppSuccess');
+};
 
 /**
  * session listener during initialization
  */
-function sessionListener(e) {
-    appendMessage('New session ID:' + e.sessionId);
-    session = e;
-    session.addUpdateListener(sessionUpdateListener);
-    session.addMessageListener(namespace, receiverMessage);
-}
+sender.sessionListener = function(e) {
+    sender.appendMessage('New session ID:' + e.sessionId);
+    sender.session = e;
+    sender.session.addUpdateListener(sender.sessionUpdateListener);
+    sender.session.addMessageListener(sender.namespace, sender.receiverMessage);
+};
 
 /**
  * listener for session updates
  */
-function sessionUpdateListener(isAlive) {
+sender.sessionUpdateListener = function(isAlive) {
     var message = isAlive ? 'Session Updated' : 'Session Removed';
-    message += ': ' + session.sessionId;
-    appendMessage(message);
+    message += ': ' + sender.session.sessionId;
+    sender.appendMessage(message);
     if (!isAlive) {
-        session = null;
+        sender.session = null;
     }
-}
-;
+};
 
 /**
  * utility function to log messages from the receiver
  * @param {string} namespace The namespace of the message
  * @param {string} message A message string
  */
-function receiverMessage(namespace, message) {
-    appendMessage("receiverMessage: " + namespace + ", " + message);
-}
-;
+sender.receiverMessage = function(namespace, message) {
+    sender.appendMessage("receiverMessage: " + namespace + ", " + message);
+};
 
 /**
  * receiver listener during initialization
  */
-function receiverListener(e) {
+sender.receiverListener = function(e) {
     if (e === 'available') {
-        appendMessage("receiver found");
+        sender.appendMessage("receiver found");
     }
     else {
-        appendMessage("receiver list empty");
+        sender.appendMessage("receiver list empty");
     }
-}
+};
 
 /**
  * stop app/session
  */
-function stopApp() {
-    session.stop(onStopAppSuccess, onError);
-}
+sender.stopApp = function() {
+    sender.session.stop(sender.onStopAppSuccess, sender.onError);
+};
 
 /**
  * send a message to the receiver using the custom namespace
  * receiver CastMessageBus message handler will be invoked
  * @param {string} message A message string
  */
-function sendMessage(message) {
-    if (session != null) {
-        session.sendMessage(namespace, message, onSuccess.bind(this, "Message sent: " + message), onError);
+sender.sendMessage = function(message) {
+    if (sender.session != null) {
+        sender.session.sendMessage(sender.namespace, message, sender.onSuccess.bind(this, "Message sent: " + message), sender.onError);
     }
     else {
         chrome.cast.requestSession(function (e) {
-            session = e;
-            session.sendMessage(namespace, message, onSuccess.bind(this, "Message sent: " + message), onError);
-        }, onError);
+            sender.session = e;
+            sender.session.sendMessage(sender.namespace, message, sender.onSuccess.bind(this, "Message sent: " + message), sender.onError);
+        }, sender.onError);
     }
-}
-
-/**
- * append message to debug message window
- * @param {string} message A message string
- */
-function appendMessage(message) {
-    console.log(message);
-    var dw = document.getElementById("debugmessage");
-    dw.innerHTML += '\n' + JSON.stringify(message);
-}
-;
+};
 
 /**
  * utility function to handle text typed in by user in the input field
  */
-function update() {
-    sendMessage(document.getElementById("input").value);
-}
+sender.update = function() {
+    sender.sendMessage(document.getElementById("input").value);
+    // TODO
+};
 
-/**
- * handler for the transcribed text from the speech input
- * @param {string} words A transcribed speech string
- */
-function transcribe(words) {
-    sendMessage(words);
-}
-
-function senderInit() {
+sender.init = function() {
     console.log("init");
     document.getElementById('backgroundColor').value = Clock.defaults.display.color.background;
     document.getElementById('activeColor').value = Clock.defaults.display.color.active;
     document.getElementById('inactiveColor').value = Clock.defaults.display.color.inactive;
     jscolor.init();
-}
+};
 
-function addEvent(el, evnt, func) {
+sender.addEvent = function(el, evnt, func) {
     if(el.addEventListener) {
         el.addEventListener(evnt, func, false);
     } else if(el.attachEvent) {
         el.attachEvent('on'+evnt, func);
     }
-}
+};
 
-addEvent(window, 'load', senderInit);
+sender.addEvent(window, 'load', sender.init);
